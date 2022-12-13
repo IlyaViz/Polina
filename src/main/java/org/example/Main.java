1629767fa1382a09056bad63d6c920837a226869
Ilya <Bezzubik12345@i.ua>
2022-12-13 16:45:28 +0200

    lesson-1 learn brackets validation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,7 @@
 package org.example;
-
-import java.nio.charset.CharsetEncoder;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.security.KeyException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String text = "((()))";
-        validateBrackets(text);
-        System.out.println(text);
-
+    public static void main(String[] args) throws KeyException {
     }
-
-    private static void validateBrackets(String a) throws Exception {
-        char[] chars = a.toCharArray();
-        int rightBrackets = 0;
-
-        for (int i = 0; i < chars.length; i++) {
-            if(chars[i] == '('){
-                rightBrackets += 1;
-            } else if(chars[i] == ')'){
-                if(rightBrackets == 0){
-                    throw new Exception("Huina peredelavay");
-                }
-                else{
-                    rightBrackets -= 1;
-                }
-            }
-
-        }
-        if(rightBrackets != 0){
-            throw new Exception("Huina peredelavay");
-        }
-
-    }
-
 }
